16732b52115eefede7ed6c007275dc27da93995b
meena26 <chavanmeena2@gmail.com>
2016-10-28 12:14:08 +0530

    project1

---
+++ welcomehelloworldupdatednew1/src/main/java/com/spring/model/Category.java
@@ -0,0 +1,42 @@
+package com.spring.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+  @Entity
+	@Table(name="bookcategories")
+	public class Category implements Serializable { // Category has list of Books
+	@Id
+	private int cid;
+	@Column(name="category")
+	private String categoryName;
+	@OneToMany(mappedBy="category")
+	List<Book> books;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public List<Book> getBooks() {
+		return books;
+	}
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+	}
+
+
+
+
